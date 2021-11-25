807de41fdc668140de8ed4b5983842adc02abc27
Martin <matovu.synergy@gmail.com>
2021-11-25 20:10:49 +0300

    Autoredirect "edit" to customer list

--- src/modules/customers/index.ts
+++ src/modules/customers/index.ts
@@ -1,9 +1,12 @@
 import CustomerIcon from '@material-ui/icons/People';
-
+import CustomerEdit from './CustomerEdit';
 import CustomerList from './CustomerList';
+import CustomerShow from './CustomerShow';
 
 const customers = {
   list: CustomerList,
+  edit: CustomerEdit,
+  show: CustomerShow,
   icon: CustomerIcon,
 };
 
